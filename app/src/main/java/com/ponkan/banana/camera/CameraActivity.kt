cf77f8895936fd02337a5968c227815c92aeb9d7
yourongcan <yourongcan@meituan.com>
2018-08-07 20:31:10 +0800

    透明状态栏

--- app/src/main/java/com/ponkan/banana/camera/CameraActivity.kt
+++ app/src/main/java/com/ponkan/banana/camera/CameraActivity.kt
@@ -1,13 +1,8 @@
 package com.ponkan.banana.camera
 
-import android.annotation.TargetApi
-import android.graphics.Color
 import android.net.Uri
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 
 import com.ponkan.banana.R
 
@@ -18,7 +13,6 @@ class CameraActivity : AppCompatActivity(), CameraFragment.OnFragmentInteraction
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
-        enableImmersive()
 
         if (cameraFragment == null) {
             cameraFragment = CameraFragment.newInstance("heng", "ha")
@@ -28,16 +22,5 @@ class CameraActivity : AppCompatActivity(), CameraFragment.OnFragmentInteraction
 
     override fun onFragmentInteraction(uri: Uri) {
 
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private fun enableImmersive() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            var flag = window.decorView.systemUiVisibility
-            flag = flag or WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
-            flag = flag or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            window.decorView.systemUiVisibility = flag
-            window.statusBarColor = Color.TRANSPARENT
-        }
     }
 }

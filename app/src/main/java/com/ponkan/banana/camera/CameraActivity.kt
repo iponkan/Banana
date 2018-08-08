940d3f630f3518e6eb334af7a5463462835cb122
yourongcan <yourongcan@meituan.com>
2018-08-08 17:54:46 +0800

    滑动才呼出状态栏，导航栏常驻

--- app/src/main/java/com/ponkan/banana/camera/CameraActivity.kt
+++ app/src/main/java/com/ponkan/banana/camera/CameraActivity.kt
@@ -3,8 +3,17 @@ package com.ponkan.banana.camera
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 import com.ponkan.banana.R
+import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+
 
 class CameraActivity : AppCompatActivity(), CameraFragment.OnFragmentInteractionListener {
 
@@ -22,5 +31,16 @@ class CameraActivity : AppCompatActivity(), CameraFragment.OnFragmentInteraction
 
     override fun onFragmentInteraction(uri: Uri) {
 
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        val mDecorView = window.decorView
+        mDecorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 }
